ee8e16f1b20e04bd8b87356a1c56c98511fb5abb
quanee <262504455@qq.com>
2020-03-19 16:14:32 +0800

    uuid router

--- main.go
+++ main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"github.com/quanee/tool/pomodoro"
+	"github.com/quanee/tool/handler"
 	"net/http"
 )
 
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
-	http.HandleFunc("/pomodoro", pomodoro.Main)
+	http.HandleFunc("/pomodoro", handler.Pomodoro)
+	http.HandleFunc("/uuid", handler.UUID)
 	http.ListenAndServeTLS(":5999", "server.crt", "server.key", nil)
 }
