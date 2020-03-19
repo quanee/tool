19e6fbb4b52ca612d350617bea7b5e5b03cf56b8
quanee <262504455@qq.com>
2020-03-19 15:13:53 +0800

    router

--- main.go
+++ main.go
@@ -8,5 +8,5 @@ import (
 func main() {
 	http.Handle("/static/", http.StripPrefix("/static/", http.FileServer(http.Dir("./static"))))
 	http.HandleFunc("/pomodoro", pomodoro.Main)
-	http.ListenAndServe(":5999", nil)
+	http.ListenAndServeTLS(":5999", "server.crt", "server.key", nil)
 }
